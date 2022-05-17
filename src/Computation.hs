7f8da1a5ae3d706c8fbb187b0d36befb55016940
Artin <95337891+Ei30metry@users.noreply.github.com>
2022-05-17 23:09:40 +0430

    singleton definititions for Intensity and Mood

--- src/Computation.hs
+++ src/Computation.hs
@@ -1,61 +1,66 @@
-{-# LANGUAGE DataKinds            #-}
-{-# LANGUAGE GADTs                #-}
-{-# LANGUAGE KindSignatures       #-}
-{-# LANGUAGE NoStarIsType         #-}
-{-# LANGUAGE OverloadedStrings    #-}
-{-# LANGUAGE PolyKinds            #-}
-{-# LANGUAGE ScopedTypeVariables  #-}
-{-# LANGUAGE TemplateHaskell      #-}
-{-# LANGUAGE TypeFamilies         #-}
-{-# LANGUAGE TypeOperators        #-}
-{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE DataKinds                #-}
+{-# LANGUAGE EmptyCase                #-}
+{-# LANGUAGE GADTs                    #-}
+{-# LANGUAGE InstanceSigs             #-}
+{-# LANGUAGE KindSignatures           #-}
+{-# LANGUAGE NoStarIsType             #-}
+{-# LANGUAGE OverloadedStrings        #-}
+{-# LANGUAGE PolyKinds                #-}
+{-# LANGUAGE ScopedTypeVariables      #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE TemplateHaskell          #-}
+{-# LANGUAGE TypeFamilies             #-}
+{-# LANGUAGE TypeOperators            #-}
+{-# LANGUAGE UndecidableInstances     #-}
 
 
 module Computation where
 
 import           Config
 import           Data.Coerce
-import           Data.Kind          (Constraint, Type)
+import           Data.Kind               (Constraint, Type)
 import           Data.List
 import           Data.Monoid
 import           Data.Semigroup
+import           Data.Singletons.Prelude
+import           Data.Singletons.TH
 import           Data.Type.Equality
 import           GHC.TypeLits
 import           Parser
 
 
--- a Type representing one's mood
-data Mood = Angry
-          | Sad
-          | Neutral
-          | Happy
-          | Excited deriving (Read, Eq, Ord, Show)
+-- a Type representing one's mood with it's singleton definitions
+singletons [d| data Mood = Angry
+                         | Sad
+                         | Neutral
+                         | Happy
+                         | Excited deriving (Read, Eq, Ord, Show) |]
 
 -- Intensity of a mood
 -- The None is not supposed to be used in a MoodReport but it is only here to
 -- serve as an mempty for our Monoid instance
-data Intensity = None
-               | Low
-               | Medium
-               | High
-               | Extreme deriving (Show, Read, Eq, Ord, Enum)
+singletons [d| data Intensity = None
+                               | Low
+                               | Medium
+                               | High
+                               | Extreme deriving (Show, Read, Eq, Ord, Enum) |]
 
 -- I'm using a newtype wrapper to be able to coerce it with the normal tuple when computing the data that comes from files
-newtype MoodReport = MR (Mood, Intensity)
+singletons [d| newtype MoodReport = MR (Mood, Intensity) |]
 
 -- write the Read instance with the help of a parser
 
-type family FromEnum (a :: Intensity) :: Nat where
-  FromEnum Low = 1
-  FromEnum Medium = 2
-  FromEnum High = 3
-  FromEnum Extreme = 4
+type family FromEnum' (a :: Intensity) :: Nat where
+  FromEnum' Low = 1
+  FromEnum' Medium = 2
+  FromEnum' High = 3
+  FromEnum' Extreme = 4
 
 -- compute the bigger Intensity
 
 -- return the ordering of two different Intensities
 type family ReturnOrdering (a :: Intensity) (b :: Intensity) :: Ordering where
-  ReturnOrdering a b = CmpNat (FromEnum a) (FromEnum b)
+  ReturnOrdering a b = CmpNat (FromEnum' a) (FromEnum' b)
 
 
 type family ReturnBiggerOne (a :: Intensity) (b :: Intensity) (c :: Ordering) :: Intensity where
@@ -81,7 +86,6 @@ instance Semigroup Intensity where
 instance Monoid Intensity where
   mappend = (<>)
   mempty = None
-
 
 
 
